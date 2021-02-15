af8d53a4708aabfbddb2c6de79140889721dc4b8
Fatimaaltameemii <78157712+Fatimaaltameemii@users.noreply.github.com>
2021-02-15 18:22:17 +0300

    logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import logo from "./diving-logo.jpg";
 function App() {
-  <title>Diving Products</title>;
+  return (
+    <div>
+      <h1>Diving Products</h1>
+      <h4>Where never an end for the sea</h4>
+      <img alt="D-logo" src={logo} />
+    </div>
+  );
 }
 
 export default App;
